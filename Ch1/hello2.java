e281af9f7665a5940ff13941c7d6bfbd89afc36c
dpeterson32-blip <dpeterson32@ivytech.edu>
2026-08-26 14:24:12 -0400

    Chapter 1

---
+++ Ch1/hello2.java
@@ -0,0 +1,4 @@
+// Filename Hello2.java
+// Written by: Dustin Peterson
+// Written on: 08/26/2026
+
---
+++ Ch1/helloDialog.java
@@ -0,0 +1,14 @@
+// Filename HelloDialog.java
+// Written by: Dustin Peterson
+// Written on: 08/26/2026
+
+import javax.swing.JOptionPane;
+
+public class helloDialog {
+    public static void main(String[] args)
+    {
+       JOptionPane.showMessageDialog(null, "Hello, World!") ;
+    
+    
+    }
+}
