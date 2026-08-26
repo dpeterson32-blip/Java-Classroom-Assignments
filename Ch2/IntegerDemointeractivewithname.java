275aada7a13a3724c96186b803fc0aca2d52e4cf
dpeterson32-blip <dpeterson32@ivytech.edu>
2026-08-26 15:43:50 -0400

    Chapter 2

--- Ch2/IntegerDemointeractivewithname.java
+++ Ch2/IntegerDemointeractivewithname.java
@@ -7,7 +7,8 @@ public class IntegerDemointeractivewithname {
         byte aByte;
         short aShort;
         long aLong;
-      
+        String name;
+        
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter an integer: ");
         anInt = input.nextInt();
@@ -17,6 +18,10 @@ public class IntegerDemointeractivewithname {
         aShort = input.nextShort();
         System.out.print("Please enter a long: ");
         aLong = input.nextLong();
+        System.out.print("Please enter your name: Dustin Peterson ");
+        input.nextLine();
+        name = input.next();
+        System.out.println("Thank you" + name);
         
     }
 }
