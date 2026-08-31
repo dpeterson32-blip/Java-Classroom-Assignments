6bc9aaf5b7184fa134b30f1aa67d55735741c246
dpeterson32-blip <dpeterson32@ivytech.edu>
2026-08-31 15:43:11 -0400

    Chapter 1-2

---
+++ Chp1-ex7/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+package Chp1-ex7;
+//Dustin Peterson
+// pg 35 08/31/2026
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("Famous quote: \"One shall stand, one shall fall.\"");
+        System.out.println("Movie: Transformers");
+        System.out.println("Character: Optimus Prime");
+        System.out.println("Year: 2007");
+    }
+}
