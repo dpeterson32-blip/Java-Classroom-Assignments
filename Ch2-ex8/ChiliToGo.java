7bfa5b19804cb45ccf164d8afe9dcbe27c7badc4
dpeterson32-blip <dpeterson32@ivytech.edu>
2026-09-02 14:36:10 -0400

    Ch2-ex8a-b

---
+++ Ch2-ex8/ChiliToGo.java
@@ -0,0 +1,32 @@
+//Dustin Peterson
+// pg 81 09/02/2026
+//ex8a
+
+import java.util.Scanner;
+
+public class ChiliToGo {
+    public static void main(String[] args) {
+        int noAdultMeals;
+        int noChildMeals;
+        double priceAdult = 7.0;
+        double priceChild = 4.0;
+        double totalAdult = 0.0;
+        double totalChild = 0.0;
+        double totalAll = 0.0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the number of childern's meals: ");
+        noChildMeals = input.nextInt();
+        System.out.print("Please enter the number of adult meals: ");
+        noAdultMeals = input.nextInt();
+        totalAdult = noAdultMeals * priceAdult;
+        totalChild = noChildMeals * priceChild;
+        totalAll = totalAdult + totalChild;
+        System.out.printf("The total cost of the adult meals is $%.2f\n", totalAdult);
+        System.out.printf("The total cost of the children's meals is: $%.2f\n", totalChild);
+        System.out.printf("The total cost of all meals is: $%.2f\n", totalAll);
+        input.close();
+
+
+
+    }
+}
