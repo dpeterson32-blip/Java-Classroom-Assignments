48486a6759168aeb4c30047cab1ac898d66657c7
dpeterson32-blip <dpeterson32@ivytech.edu>
2026-09-04 14:10:14 -0400

    Chapter 3

--- Ch3/ParadiseInfo2.java
+++ Ch3/ParadiseInfo2.java
@@ -1,9 +1,34 @@
+//DustinPeterson
+//pg 89 09/02/2026
 import java.util.Scanner;
-
 public class ParadiseInfo2 {
-    
-    public static void main(String[] args){
-       Scanner input = new Scanner (System.in);
-
+    public static void main(String[] args) 
+    { 
+        displayInfo();
+      double price;
+      double discount;
+      double savings;
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Enter cutoff price for discount >>");
+      price = keyboard.nextDouble();
+      System.out.print("Enter discount rate as a whole number >>");
+      discount = keyboard.nextDouble();
+      savings = computeDiscountInfo(price, discount);
+      System.out.println("Special this week on any service over" + price);
+      System.out.println("Discount of" + discount + "percent");
+      System.out.println("That's a savings of at least $" + savings);
     }
+
+    public static double computeDiscountInfo(double price, double discountRate){
+      double savings;
+      savings = price * discountRate / 100;
+      return savings;
+    }
+
+    public static void displayInfo()
+    {
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
+    }
+
 }
