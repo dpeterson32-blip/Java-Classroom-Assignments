66a92fcf6371eee8d38a78fa7a135f6701dcdf59
dpeterson32-blip <dpeterson32@ivytech.edu>
2026-09-09 13:51:49 -0400

    Chapter 3- ex5

---
+++ Ch3-ex5/BookstoreCredit.java
@@ -0,0 +1,13 @@
+//Dustin Peterson
+// pg. 112 09/09/2026
+import java.until.Scanner;
+
+public class BookstoreCredit {
+    public static void main(String[] args) {
+        double gpa;
+        double credit;
+        Scanner input = new Scanner(System.in);
+        System.out.print
+
+    }
+}
