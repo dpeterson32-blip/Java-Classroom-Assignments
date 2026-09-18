a6fe167c01d3cbe49d468c1fc72e2e8c2b15bf5f
dpeterson32-blip <dpeterson32@ivytech.edu>
2026-09-18 12:55:19 -0400

    Chapter 4-ex1a and 1b

---
+++ chp4-ex1a and 1b/Sandwich.java
@@ -0,0 +1,24 @@
+//Dustin Peterson
+//pg156 09/18/2026
+
+public class Sandwich {
+    private String ingredient;
+    private String breadType;
+    private double price;
+    public void setIngredient(String ing){
+        ingredient =ing;
+    }
+    public void setBreadType(String bt){
+        breadType = bt;
+    }
+    public String getIngredient(){
+        return ingredient;
+    }
+    public String getBreadType(){
+        return breadType;
+    }
+    public double getPrice(){
+        return price;
+    }
+    
+}
---
+++ chp4-ex1a and 1b/TestSandwich.java
@@ -0,0 +1,13 @@
+//Dustin Peterson
+//pg156 09/18/2026
+import java.util.Scanner;
+
+public class TestSandwich {
+    public static void main(String[] args) {
+        Sandwich firstSandwich = new Sandwich();
+        firstSandwich = getData(firstSandwich);
+        System.out.println("You ordered a: ");
+        System.out.println()
+        
+    }
+}
